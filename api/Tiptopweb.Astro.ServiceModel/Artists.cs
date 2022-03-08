ceadf3fe16ec5cca4b4c64987753b58149ca1119
Thierry Lefebvre <thierry@tiptopweb.com.au>
2022-03-08 12:59:26 +1100

    Replace Id bu Slug for Artist pages

---
+++ api/Tiptopweb.Astro.ServiceModel/Artists.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+using ServiceStack;
+using ServiceStack.DataAnnotations;
+using ServiceStack.Model;
+
+namespace Tiptopweb.Astro.ServiceModel;
+
+[Route("/find-artists")]
+public class FindArtists : QueryDb<Artist>
+{
+    
+}
---
+++ api/Tiptopweb.Astro.ServiceModel/Dtos.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+
+using ServiceStack.DataAnnotations;
+
+namespace Tiptopweb.Astro.ServiceModel;
+
+[Alias("Albums")]
+public class Album
+{
+    [AutoIncrement]
+    public long AlbumId { get; set; }
+
+    [Required]
+    public string Title { get; set; }
+
+    public long ArtistId { get; set; }
+
+    [Reference]
+    public List<Track> Tracks { get; set; } 
+}
+
+[Alias("Artists")]
+public class Artist
+{
+    [AutoIncrement]
+    public long ArtistId { get; set; }
+
+    public string Name { get; set; }
+    
+    [Reference]
+    public List<Album> Albums { get; set; } 
+}
+
+[Alias("Tracks")]
+public class Track
+{
+    [AutoIncrement]
+    public long TrackId { get; set; }
+
+    [Required]
+    public string Name { get; set; }
+
+    public long? AlbumId { get; set; }
+    public long MediaTypeId { get; set; }
+    public long? GenreId { get; set; }
+    public string Composer { get; set; }
+    public long Milliseconds { get; set; }
+    public long? Bytes { get; set; }
+    public decimal UnitPrice { get; set; }
+}
