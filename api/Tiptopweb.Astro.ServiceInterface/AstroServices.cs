7d1bbc53ab64f2aa50dab4857aee84b46c84b886
Thierry Lefebvre <thierry@tiptopweb.com.au>
2022-03-21 16:57:26 +1100

    gh-pages branch

--- api/Tiptopweb.Astro.ServiceInterface/AstroServices.cs
+++
@@ -1,9 +0,0 @@
-using ServiceStack;
-
-namespace Tiptopweb.Astro.ServiceInterface;
-
-public partial class AstroServices : Service
-{
-    // for Custom AutoQuery
-    public IAutoQueryDb AutoQuery { get; set; }
-}
--- api/Tiptopweb.Astro/wwwroot/load.e4960eae.js
+++
@@ -1,1 +0,0 @@
-import{h as f}from"./chunks/vendor.f5f68df4.js";export{f as default};
